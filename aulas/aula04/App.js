b164e3455e8ac40a5d75333f51f8939a1140f5cb
unknown <2324290035@ACADEMICO.IESB.BR>
2025-03-31 21:50:37 -0300

    pratica 05

--- aulas/aula04/App.js
+++ aulas/aula04/App.js
@@ -1,11 +1,12 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import LoginScreen from "./screens/LoginScreen";
 import ContactScreen from "./screens/ContactScreen";
+import TaskScreen from "./screens/TaskScreen";
 
 function App() {
   return (
     <SafeAreaProvider>
-        <ContactScreen/>
+        <TaskScreen/>
     </SafeAreaProvider>
   )
 }
